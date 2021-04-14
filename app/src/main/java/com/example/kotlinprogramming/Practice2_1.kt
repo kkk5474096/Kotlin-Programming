38f33b85b3aeb65e3416a992c996da5d85822bc9
Leekangmin <kkk5474096@naver.com>
2021-04-14 17:01:48 +0900

    Practice2.1.+

--- app/src/main/java/com/example/kotlinprogramming/Practice2_1.kt
+++ app/src/main/java/com/example/kotlinprogramming/Practice2_1.kt
@@ -13,6 +13,9 @@ fun main() {
     println(calculator2.mul(4,3,2))
     println(calculator2.division(10,2,1))
 
+    val calculator3 = Calculator3(4)
+    calculator3.add(8).sub(3)
+
 }
 
 class Calculator() {
@@ -67,4 +70,28 @@ class Calculator2() {
         }
         return result
     }
+}
+
+class Calculator3(val initialValue: Int) {
+
+    fun add(number: Int): Calculator3 {
+        val result = this.initialValue + number
+        return Calculator3(result)
+    }
+
+    fun sub(number: Int): Calculator3 {
+        val result = this.initialValue - number
+        return Calculator3(result)
+    }
+
+    fun mul(number: Int): Calculator3 {
+        val result = this.initialValue * number
+        return Calculator3(result)
+    }
+
+    fun division(number: Int): Calculator3 {
+        val result = this.initialValue / number
+        return Calculator3(result)
+    }
+
 }

a34cb10a54587b27514bceed59f90186da2d90e6
Leekangmin <kkk5474096@naver.com>
2021-04-14 16:31:52 +0900

    Practice2.1

---
+++ app/src/main/java/com/example/kotlinprogramming/Practice2_1.kt
@@ -0,0 +1,70 @@
+package com.example.kotlinprogramming
+
+fun main() {
+    val calculator = Calculator()
+    println(calculator.add(4,3))
+    println(calculator.sub(4,3))
+    println(calculator.mul(4,3))
+    println(calculator.division(4,3))
+
+    val calculator2 = Calculator2()
+    println(calculator2.add(4,3,2))
+    println(calculator2.sub(4,3,3))
+    println(calculator2.mul(4,3,2))
+    println(calculator2.division(10,2,1))
+
+}
+
+class Calculator() {
+    fun add(a: Int, b: Int): Int{
+        return a + b
+    }
+    fun sub(a: Int, b: Int): Int{
+        return a - b
+    }
+
+    fun mul(a: Int, b: Int): Int{
+        return a * b
+    }
+    fun division(a: Int, b: Int): Int{
+        return a / b
+    }
+}
+
+class Calculator2() {
+    fun add(vararg numbers: Int): Int {
+        var result = 0
+        numbers.forEach {
+            result += it
+        }
+        return result
+    }
+
+    fun sub(vararg numbers: Int): Int {
+        var result: Int = numbers[0]
+        for (i in 0 until numbers.size) {
+            if(i != 0) {
+                result -= numbers[i]
+            }
+        }
+        return result
+    }
+
+    fun mul(vararg numbers: Int): Int {
+        var result = 1
+        numbers.forEach {
+            result *= it
+        }
+        return result
+    }
+
+    fun division(vararg numbers: Int): Int {
+        var result = numbers[0]
+        for (i in 0 until numbers.size) {
+            if( i != 0) {
+                result /= numbers[i]
+            }
+        }
+        return result
+    }
+}

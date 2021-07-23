eb314851d0d3ab1fb15bc487078d43f611054b66
Leekangmin <kkk5474096@naver.com>
2021-07-23 16:26:40 +0900

    ViewModel 실습 초기 셋팅

---
+++ app/src/main/java/com/example/kotlinprogramming/screen/game/GameViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinprogramming.screen.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+    init {
+        Log.d("GameViewModel", "GameViewModel created")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameViewModel", "GameViewModel destroyed")
+    }
+}
---
+++ app/src/main/java/com/example/kotlinprogramming/screen/score/ScoreFragment.kt
@@ -0,0 +1,30 @@
+package com.example.kotlinprogramming.screen.score
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.kotlinprogramming.R
+import com.example.kotlinprogramming.databinding.ScoreFragmentBinding
+
+class ScoreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate view and obtain an instance of the binding class.
+        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.score_fragment,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+}

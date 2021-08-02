3bdaab030b265d6ddf8c0f411053ff1295a44f6f
Leekangmin <kkk5474096@naver.com>
2021-08-02 17:32:32 +0900

    ScoreViewModel LiveData 변경

--- app/src/main/java/com/example/kotlinprogramming/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/kotlinprogramming/screens/score/ScoreViewModel.kt
@@ -1,14 +1,19 @@
 package com.example.kotlinprogramming.screens.score
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel(finalScore: Int): ViewModel() {
 
-    var score = finalScore
+    private val _score =  MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     init {
         Log.d("ScoreViewModel", "Final score is $finalScore")
+        _score.value = finalScore
     }
 
 }

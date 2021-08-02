0dba5c32e224e0ac7408434a22241fe2b4a43c73
Leekangmin <kkk5474096@naver.com>
2021-08-02 17:35:38 +0900

    ScoreViewModel LiveData 다시시작 구현

--- app/src/main/java/com/example/kotlinprogramming/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/kotlinprogramming/screens/score/ScoreViewModel.kt
@@ -11,6 +11,18 @@ class ScoreViewModel(finalScore: Int): ViewModel() {
     val score: LiveData<Int>
         get() = _score
 
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
+    }
+
     init {
         Log.d("ScoreViewModel", "Final score is $finalScore")
         _score.value = finalScore

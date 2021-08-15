ac1ef302cce1d55ff90b28db2b6f00cc66cf0ff6
Leekangmin <kkk5474096@naver.com>
2021-08-15 23:44:55 +0900

    GameFragment 데이터바인딩 수정

--- app/src/main/java/com/example/kotlinprogramming/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/kotlinprogramming/screens/game/GameFragment.kt
@@ -37,12 +37,10 @@ class GameFragment : Fragment() {
 
         binding.gameViewModel = viewModel
 
+        binding.lifecycleOwner = viewLifecycleOwner
+
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
-        })
-
-        viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
-            binding.wordText.text = newWord
         })
 
         viewModel.eventGameFinish.observe(viewLifecycleOwner, Observer<Boolean> { hasFinished ->
